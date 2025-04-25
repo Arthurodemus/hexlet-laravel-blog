46340a2778f8fadc3bfd91c7f35c993d654c1254
fox <a.foks@parisnail.ru>
2025-04-25 14:23:00 +0300

    Обработчик страницы

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PageController;
+
+Route::get('about', [PageController::class, 'about'])->name('about');
 
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('about', function () {
-    return view('about');
-})->name('about');
